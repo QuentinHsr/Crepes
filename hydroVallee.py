8d9f25f5d1a48017177ab3c42455700eba3263b7
Bafouye <46017913+Bafouye@users.noreply.github.com>
2018-12-22 13:17:28 +0100

    updates

--- hydroVallee.py
+++ hydroVallee.py
@@ -129,70 +129,6 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
     body = json.dumps([{'nom':n, 'lat':lat, 'lon': lon, 'cd':cd} for (lon,lat,n,cd) in r])
     self.send(body,headers)
 
-  #
-  # On génère et on renvoie un graphique de ponctualite (cf. TD1)
-  #
-  def send_ponctualite(self):
-
-    conn = sqlite3.connect('ter.sqlite')
-    c = conn.cursor()
-    
-    if len(self.path_info) <= 1 or self.path_info[1] == '' :   # pas de paramètre => liste par défaut
-        # Definition des régions et des couleurs de tracé
-        regions = [("Rhône Alpes","blue"), ("Auvergne","green"), ("Auvergne-Rhône Alpes","cyan"), ('Bourgogne',"red"), 
-                   ('Franche Comté','orange'), ('Bourgogne-Franche Comté','olive') ]
-    else:
-        # On teste que la région demandée existe bien
-        c.execute("SELECT DISTINCT Région FROM 'regularite-mensuelle-ter'")
-        reg = c.fetchall()
-        if (self.path_info[1],) in reg:   # Rq: reg est une liste de tuples
-          regions = [(self.path_info[1],"blue")]
-        else:
-            print ('Erreur nom')
-            self.send_error(404)    # Région non trouvée -> erreur 404
-            return None
-    
-    # configuration du tracé
-    fig1 = plt.figure(figsize=(18,6))
-    ax = fig1.add_subplot(111)
-    ax.set_ylim(bottom=80,top=100)
-    ax.grid(which='major', color='#888888', linestyle='-')
-    ax.grid(which='minor',axis='x', color='#888888', linestyle=':')
-    ax.xaxis.set_major_locator(pltd.YearLocator())
-    ax.xaxis.set_minor_locator(pltd.MonthLocator())
-    ax.xaxis.set_major_formatter(pltd.DateFormatter('%B %Y'))
-    ax.xaxis.set_tick_params(labelsize=10)
-    ax.xaxis.set_label_text("Date")
-    ax.yaxis.set_label_text("% de régularité")
-            
-    # boucle sur les régions
-    for l in (regions) :
-        c.execute("SELECT * FROM 'regularite-mensuelle-ter' WHERE Région=? ORDER BY Date",l[:1])  # ou (l[0],)
-        r = c.fetchall()
-        # recupération de la date (colonne 2) et transformation dans le format de pyplot
-        x = [pltd.date2num(dt.date(int(a[1][:4]),int(a[1][5:]),1)) for a in r if not a[7] == '']
-        # récupération de la régularité (colonne 8)
-        y = [float(a[7]) for a in r if not a[7] == '']
-        # tracé de la courbe
-        plt.plot(x,y,linewidth=1, linestyle='-', marker='o', color=l[1], label=l[0])
-        
-    # légendes
-    plt.legend(loc='lower left')
-    plt.title('Régularité des TER (en %)',fontsize=16)
-
-    # génération des courbes dans un fichier PNG
-    fichier = 'courbes/ponctualite_'+self.path_info[1] +'.png'
-    plt.savefig('client/{}'.format(fichier))
-    plt.close()
-    
-    #html = '<img src="/{}?{}" alt="ponctualite {}" width="100%">'.format(fichier,self.date_time_string(),self.path)
-    body = json.dumps({
-            'title': 'Régularité TER '+self.path_info[1], \
-            'img': '/'+fichier \
-             });
-    # on envoie
-    headers = [('Content-Type','application/json')];
-    self.send(body,headers)
 
             
   def send_debits(self):
@@ -204,22 +140,23 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
         # Definition des régions et des couleurs de tracé
 #        regions = [("Rhône Alpes","blue"), ("Auvergne","green"), ("Auvergne-Rhône Alpes","cyan"), ('Bourgogne',"red"), 
 #                   ('Franche Comté','orange'), ('Bourgogne-Franche Comté','olive') ]
-        pass
+        station = [("J1711710","blue")]
+        self.path_info[1]="inconnu"
     else:
         # On teste que la région demandée existe bien
-        c.execute("SELECT DISTINCT code_hydro FROM 'hydro_historique'")
+        c.execute("SELECT code_hydro FROM 'hydro_historique'")
         codes = c.fetchall()
-        if (self.path_info[1],) in codes:   # Rq: reg est une liste de tuples
+        if (self.path_info[1],) in codes:   # Rq: codes est une liste de tuples
           station = [(self.path_info[1],"blue")]
         else:
             print ('Erreur nom')
-            self.send_error(404)    # Région non trouvée -> erreur 404
+            self.send_error(404)    # Station non trouvée -> erreur 404
             return None
     
     # configuration du tracé
     fig1 = plt.figure(figsize=(18,6))
     ax = fig1.add_subplot(111)
-    ax.set_ylim(bottom=80,top=100)
+    ax.set_ylim(bottom=0,top=10)
     ax.grid(which='major', color='#888888', linestyle='-')
     ax.grid(which='minor',axis='x', color='#888888', linestyle=':')
     ax.xaxis.set_major_locator(pltd.YearLocator())
@@ -231,12 +168,12 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
             
     # boucle sur les régions
     for l in (station) :
-        c.execute("SELECT debit_donnee_validee_m3 FROM 'hydro_historique' WHERE code_hydro=? ORDER BY date",l[:1])  # ou (l[0],)
+        c.execute("SELECT debit_donnee_validee_m3,date FROM 'hydro_historique' WHERE code_hydro=? ORDER BY date",l[:1])  # ou (l[0],)
         r = c.fetchall()
         # recupération de la date (colonne 2) et transformation dans le format de pyplot
-        x = [pltd.date2num(dt.date(int(a[1][:4]),int(a[1][5:]),1)) for a in r if not a[7] == '']
+        x = [pltd.date2num(dt.date(int(a[1][:4]),int(a[1][5:7]),int(a[1][8:]))) for a in r if not a[0] == '']
         # récupération des débits (colonne 8)
-        y = [float(a[7]) for a in r if not a[7] == '']
+        y = [float(a[0]) for a in r if not a[0] == '']
         # tracé de la courbe
         plt.plot(x,y,linewidth=1, linestyle='-', marker='o', color=l[1], label=l[0])
         
