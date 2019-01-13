433d34a6b7a88166cbfa177803d9de3910a8b707
Bafouye <46017913+Bafouye@users.noreply.github.com>
2019-01-13 20:53:34 +0100

    Update testSQLvallee.py

--- testSQLvallee.py
+++ testSQLvallee.py
@@ -18,20 +18,44 @@ import sqlite3
 conn = sqlite3.connect('Crepes.sqlite')
 c = conn.cursor()
 
-c.execute("SELECT X,Y,LbStationHydro,CdStationHydroAncienRef FROM 'Stations'")
-r = c.fetchall()
-#On vérifie que des données sur la station sont diponibles dans la table de données
-c.execute("SELECT DISTINCT code_hydro FROM 'hydro_historique'")
-ac = c.fetchall()    
-compt=0
-for i in range(len(r)):
-    if (r[i][3],) in ac:
-        r[i]+=(1,)
-        print(r[i][3])
+#c.execute("SELECT X,Y,LbStationHydro,CdStationHydroAncienRef FROM 'Stations'")
+#r = c.fetchall()
+##On vérifie que des données sur la station sont diponibles dans la table de données
+#c.execute("SELECT DISTINCT code_hydro FROM 'hydro_historique'")
+#ac = c.fetchall()    
+#compt=0
+#for i in range(len(r)):
+#    if (r[i][3],) in ac:
+#        r[i]+=(1,)
+#        print(r[i][3])
+#
+#    else:
+#        r[i]+=(0,)
+#        
+#headers = [('Content-Type','application/json')];
+#body = json.dumps([{'nom':n, 'lat':lat, 'lon': lon, 'cd':cd, 'datAv': datAv}\
+#                   for (lon,lat,n,cd,datAv) in r])
 
-    else:
-        r[i]+=(0,)
-        
-headers = [('Content-Type','application/json')];
-body = json.dumps([{'nom':n, 'lat':lat, 'lon': lon, 'cd':cd, 'datAv': datAv}\
-                   for (lon,lat,n,cd,datAv) in r])
+
+
+
+d1="'{}-{}-{}'".format('2018','01','04') 
+d2="'{}-{}-{}'".format('2018','01','10')
+
+c.execute("SELECT debit_donnee_validee_m3,date FROM (SELECT debit_donnee_validee_m3,date FROM hydro_historique  WHERE code_hydro=?) WHERE date BETWEEN {} and {} ORDER BY date".format(d1,d2),("'J1711710'",))  # ou (l[0],)
+r = c.fetchall()
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
